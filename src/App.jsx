5b6fa61de4cfc7fc5b71c0caf69c32023f23ad32
David Islam <david.islam24@gmail.com>
2024-01-23 19:45:58 -0800

    Create cards array and implement shuffleCards()

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';  
+import { v4 as uuidv4 } from 'uuid';
 import './App.css'
 
+const cardImages = [
+  { "src": "./assets/helmet-1.png" },
+  { "src": "./assets/potion-1.png" },
+  { "src": "./assets/ring-1.png" },
+  { "src": "./assets/scroll-1.png" },
+  { "src": "./assets/shield-1.png" },
+  { "src": "./assets/sword-1.png" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0); 
+
+  const shuffleCards = () => {
+    // Duplicate the set of cards, randomly sort them, and tag on an id
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ( { ...card, id: uuidv4() } ));
+    setCards(shuffledCards);  
+    setTurns(0);
+  }
+
   return (
     <div className='App'>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   )
 }

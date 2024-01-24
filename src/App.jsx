67e8fd99e67acddd78df78c39931747857e25dcf
David Islam <david.islam24@gmail.com>
2024-01-23 23:30:26 -0800

    Create card grid

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,14 @@ function App() {
     <div className='App'>
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <img className="front" src={card.src} alt="card front" />
+            <img className="back" src="./assets/cover.png" alt="card back" />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
